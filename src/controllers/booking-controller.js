6a749bcfe283920575c8434ed885cd6c7c289588
abhishek kamat <abhishekkamat142@gmail.com>
2025-01-15 15:40:48 +0530

    Added rabbit mq config

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,30 +1,31 @@
 const { StatusCodes } = require('http-status-codes');
 
 const { BookingService } = require('../service/index');
+const { createChannel } = require('../utils/messageQueue');
 
 const bookingService = new BookingService();
-const create = async(req, res) => {
-    try {
-        const response = await bookingService.createBooking(req.body);
-        return res.status(StatusCodes.OK).json({
-            message: 'Booking created successfully',
-            success: true,
-            data: response,
-            err: {}
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(error.statusCode).json({
-            message: error.message,
-            success: false,
-            err: error.explanation,
-            data: {}
-        });
+
+class BookingController{
+    async create(req, res) {
+        try {
+            const response = await bookingService.createBooking(req.body);
+            return res.status(StatusCodes.OK).json({
+                message: 'Booking created successfully',
+                success: true,
+                data: response,
+                err: {}
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(error.statusCode).json({
+                message: error.message,
+                success: false,
+                err: error.explanation,
+                data: {}
+            });
+        }
+        
     }
-    
 }
 
-
-module.exports = {
-    create
-}
+module.exports = BookingController;
